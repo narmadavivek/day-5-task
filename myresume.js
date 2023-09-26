05f268b918bb15b6439b6c415a609ef21a24f322
narmadavivek <145418529+narmadavivek@users.noreply.github.com>
2023-09-26 08:51:37 -0700

    Add files via upload

---
+++ myresume.js
@@ -0,0 +1,48 @@
+let myresume={
+    "basics":{
+        "name":"narmada v",
+        "email":"narmada.viveka@gmail.com",
+        "phone":7633466789,
+        "degree":"B.E",
+        "location":{
+            "address":"3/450 A,a pace east appartment",
+            "postalcode":6,
+            "city":"chennai",
+            "state":"tamil nadu",
+            "country":"india"
+        },
+    
+     "profiles":[
+        {
+         "github":"https://github.com/narmadavivek"
+        }
+     ]
+    },
+    "education":[
+        {
+            "institution":"jeppiaar srr engineering college",
+            "department":"electronics and communication engineering",
+            "study type":"full time",
+            "batch start year":2016,
+            "batch end year":2020,
+            "gpa":7.43,
+       }
+    ],
+    "skills":[
+        {
+            "skills name":"javascript",
+            "level":"beginer",
+        }
+    ],
+    "languages":[
+        {
+            "languages":"tamil,english",
+        }
+    ],
+    "interest":[
+        {
+            "name":"story writing, music",
+        }
+    ],
+}
+console.log(myresume);
